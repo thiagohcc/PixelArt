72df7acffb855efe6f57c5cfdd9cb8331221d477
thiagohcc <dzno23@gmail.com>
2022-08-31 16:17:35 -0300

    Passei o script para carregar todo após o window.onload

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-// Captura os elementos no DOM
-const buttonRandomColor = document.getElementById('button-random-color');
-const colorsInPalette = document.getElementsByClassName('color');
-// Gerador de cores aleatórias excluindo o braco e o preto.
-function colorGenerator() {
+window.onload = function() {
+  // Captura os elementos no DOM
+  const buttonRandomColor = document.getElementById('button-random-color');
+  const colorsInPalette = document.getElementsByClassName('color');
+
+  // Gerador de cores aleatórias excluindo o branco e o preto.
+  function colorGenerator() {
     const hexadecimalCharacteres = '0123456789ABCDEF';
     let color = '#';
     for (let index = 0; index < 6; index += 1) {
-        color += hexadecimalCharacteres[Math.floor(Math.random()* 16)];
+      color += hexadecimalCharacteres[Math.floor(Math.random()* 16)];
     }
     if (color === '#FFFFFF' || color === '#000000') {
-        for (let index = 0; index < 6; index += 1) {
-            color += hexadecimalCharacteres[Math.floor(Math.random()* 16)];
-        }     
+      for (let index = 0; index < 6; index += 1) {
+          color += hexadecimalCharacteres[Math.floor(Math.random()* 16)];
+      }     
     }  
     return color;
-}
-// cria a função do botão gerador de cores aleatórias
-for (let index = 1; index < colorsInPalette.length; index += 1) {
+  }
+
+  // cria a função do botão gerador de cores aleatórias e salvaelas no localStorage
+  for (let index = 1; index < colorsInPalette.length; index += 1) {
     let randomColor = colorsInPalette[index];
-    console.log(randomColor);
     buttonRandomColor.addEventListener('click', selectRandomColors);
 
     function selectRandomColors() {
-        randomColor.style.backgroundColor = colorGenerator();
-        console.log(randomColor.style.backgorundColor);
+      randomColor.style.backgroundColor = colorGenerator();
     }
+  }
 }
+
+
+
