7d853a3c9b36fc4157278177f10cadc5cb63aedf
thiagohcc <dzno23@gmail.com>
2022-09-01 19:27:35 -0300

    Reiniciei o projeto do zero - Concluido até o item 4

--- script.js
+++ script.js
@@ -1,75 +1,55 @@
-// Captura os elementos no DOM
-const buttonRandomColor = document.getElementById('button-random-color');
-const colorsInPalette = document.getElementsByClassName('color');
-const pixelBoard = document.getElementById('pixel-board');
-const pixelLine = document.getElementsByClassName('pixel-line');
-const pixel = document.getElementsByClassName('pixel');
-let numberOfPixelLines = 5;
-let numberOfColorsInColorPalletes = 4;
+// Elementos capturados pelo DOM
+const title = document.getElementById('title');
+const cores = document.getElementsByClassName('color');
 
-// Carrega as cores do localStorage
-window.onload = function() {
-  let lS = JSON.parse(localStorage.getItem('color'));
-  for (let index = 1; index < colorsInPalette.length; index += 1) {
-    let randomColor = colorsInPalette[index];
-    let oldColor = lS[index - 1];
-    randomColor.style.backgroundColor = oldColor;
-  }
-  colorsInPalette[0].className = 'color selected';
+// Variaveis de ajuste de quantidades
+let numeroDeCoresNaPaleta = 4;
+
+// Início do código
+
+// Cria a section do color-palette
+const paletaDeCores = document.createElement('section');
+paletaDeCores.className = 'color-palette';
+title.appendChild(paletaDeCores);
+
+// Cria o espaço para as quatro cores na paleta de cores
+for (let index = 1; index <= numeroDeCoresNaPaleta; index += 1) {
+  let color = document.createElement('div');
+  color.className = 'color';
+  paletaDeCores.appendChild(color);
 }
 
-// Gerador de cores aleatórias excluindo o branco e o preto.
-function colorGenerator() {
-  const hexadecimalCharacteres = '0123456789ABCDEF';
-  let color = '#';
+// Colore  inicialemnte a paleta de cores
+const cor = ['rgb(0, 0, 0)', 'rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)']
+for (let index = 0; index < cores.length; index += 1) {
+  cores[index].style.backgroundColor = cor[index];
+}
+
+// Cria o botão para gerar cores aleatŕoias
+const botao = document.createElement('button');
+botao.id = 'button-random-color';
+botao.innerText = 'Cores aleatórias';
+paletaDeCores.appendChild(botao);
+
+// Gerador de cores aleatórias
+function geradorDeCores() {
+  const caracteresHexadecimais = '0123456789ABCDEF';
+  let corAleatoria = '#';
   for (let index = 0; index < 6; index += 1) {
-    color += hexadecimalCharacteres[Math.floor(Math.random()* 16)];
+    corAleatoria += caracteresHexadecimais[Math.floor(Math.random() * 16)];
   }
-  if (color === '#FFFFFF' || color === '#000000') {
+  if (corAleatoria === '#FFFFFF' || corAleatoria === '#000000') {
     for (let index = 0; index < 6; index += 1) {
-        color += hexadecimalCharacteres[Math.floor(Math.random()* 16)];
-    }     
-  }  
-  return color;
-}
-
-// cria a função do botão gerador de cores aleatórias e salva elas no localStorage
-let color = [];
-for (let index = 1; index < colorsInPalette.length; index += 1) {
-  let randomColor = colorsInPalette[index];
-  buttonRandomColor.addEventListener('click', selectRandomColors);
-  function selectRandomColors() {
-    randomColor.style.backgroundColor = colorGenerator();
-    color.push(randomColor.style.backgroundColor);
-    localStorage.color = JSON.stringify(color);
-  }
-}
-
-// Aplica a class selected na cor escolhida
-for (let index = 0; index < colorsInPalette.length; index += 1) {
-  let color = colorsInPalette[index];
-  color.addEventListener('click', function() {
-    if (color.className === 'color') {
-      for (let index = 0; index < colorsInPalette.length; index += 1) {
-        if (colorsInPalette[index] != color) {
-          colorsInPalette[index].className = 'color';
-        }
-      }
-      color.className = 'color selected';
-      console.log(color.style.backgroundColor);
+      corAleatoria += caracteresHexadecimais[Math.floor(Math.random() * 16)];
     }
-  })
+  }
+  return corAleatoria;
 }
 
-// Aplica a cor escolhida no pixel selecionado
-for (let index = 0; index < pixel.length; index += 1) {
-  let selectedPixel = pixel[index];
-  let selectedColor = document.getElementsByClassName('color selected');
-  selectedPixel.addEventListener('click', colorPixel);
-  function colorPixel() {
-    let bgColorSelected = selectedColor[0].style.backgroundColor;
-    selectedPixel.style.backgroundColor = bgColorSelected;
+// Faz o botão gerar cores aleatórias na paletade cores
+botao.addEventListener('click', criaCoresAleatorias);
+function criaCoresAleatorias() {
+  for (let index = 1; index < numeroDeCoresNaPaleta; index += 1) {
+    cores[index].style.backgroundColor = geradorDeCores();
   }
 }
-
-
